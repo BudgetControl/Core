f684c6d36b3fb1aaccbe97120124de1ddd897014
Marco De Felice <marco.defelice@consulenti.eolo.it>
2023-05-10 18:04:34 +0200

    developed unit test

--- database/migrations/2023_05_10_124223_alter_table_users_change_email.php
+++ database/migrations/2023_05_10_124223_alter_table_users_change_email.php
@@ -4,11 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-Schema::table('users', function (Blueprint $table) {
-    $table->string('email',255)->change();
-});
 
-Schema::table('personal_access_tokens', function (Blueprint $table) {
-    $table->dropIndex('token');
-    $table->string('token',255)->change();
-});
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email',255)->change();
+        });
+        
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->string('token',255)->change();
+            $table->dropIndex('personal_access_tokens_token_unique');
+        });
+    }
+};
