eafd7952a36af71a68d6e31a2363c74ea1ec400d
Marco De Felice <marco.defelice@consulenti.eolo.it>
2023-06-01 10:54:13 +0200

    develop new hot feture

--- app/Stats/Controllers/StatsController.php
+++ app/Stats/Controllers/StatsController.php
@@ -29,12 +29,12 @@ class StatsController extends Controller
     private string $startDatePassed;
     private string $endDatePassed;
 
-    public function __construct() 
+    public function __construct()
     {
         $this->startDate = date('Y/m/d H:i:s', time());
         $this->endDate = date('Y/m/d H:i:s', time());
     }
-    
+
     /**
      * set data to start stats
      * @param string $date
@@ -61,7 +61,7 @@ class StatsController extends Controller
     public function setDateEnd(string $date): self
     {
         $this->endDate = $date;
-        
+
         $passedDateTime = new DateTime($date);
         $passedDateTime = $passedDateTime->modify('-1 month');
         $this->endDatePassed = $passedDateTime->format('Y-m-d H:i:s');
@@ -73,7 +73,7 @@ class StatsController extends Controller
      * retrive data
      * @param bool $planning
      * 
-	 * @return JsonResponse
+     * @return JsonResponse
      */
     public function incoming(bool $planning): JsonResponse
     {
@@ -83,24 +83,25 @@ class StatsController extends Controller
         $entryOld = new IncomingService();
         $entryOld->setDateStart($this->startDatePassed)->setDateEnd($this->endDatePassed);
 
-        if($planning === true) {
+        if ($planning === true) {
             $entry->setPlanning($planning);
             $entryOld->setPlanning($planning);
         } else {
             $entry->setPlanning(false);
         }
 
-        return response()->json(new ResponseService(
-            $this->buildResponse($entry->get(),$entryOld->get()))
+        return response()->json(
+            new ResponseService(
+                $this->buildResponse($entry->get(), $entryOld->get())
+            )
         );
-        
     }
 
     /**
      * retrive data
      * @param bool $planning
      * 
-	 * @return JsonResponse
+     * @return JsonResponse
      */
     public function expenses(bool $planning): JsonResponse
     {
@@ -110,24 +111,25 @@ class StatsController extends Controller
         $entryOld = new ExpensesService();
         $entryOld->setDateStart($this->startDatePassed)->setDateEnd($this->endDatePassed);
 
-        if($planning === true) {
+        if ($planning === true) {
             $entry->setPlanning($planning);
             $entryOld->setPlanning($planning);
         } else {
             $entry->setPlanning(false);
         }
 
-        return response()->json(new ResponseService(
-            $this->buildResponse($entry->get(),$entryOld->get()))
+        return response()->json(
+            new ResponseService(
+                $this->buildResponse($entry->get(), $entryOld->get())
+            )
         );
-        
     }
 
     /**
      * retrive data
      * @param bool $planning
      * 
-	 * @return JsonResponse
+     * @return JsonResponse
      */
     public function transfer(bool $planning): JsonResponse
     {
@@ -137,24 +139,25 @@ class StatsController extends Controller
         $entryOld = new TransferService();
         $entryOld->setDateStart($this->startDatePassed)->setDateEnd($this->endDatePassed);
 
-        if($planning === true) {
+        if ($planning === true) {
             $entry->setPlanning($planning);
             $entryOld->setPlanning($planning);
         } else {
             $entry->setPlanning(false);
         }
 
-        return response()->json(new ResponseService(
-            $this->buildResponse($entry->get(),$entryOld->get()))
+        return response()->json(
+            new ResponseService(
+                $this->buildResponse($entry->get(), $entryOld->get())
+            )
         );
-        
     }
 
     /**
      * retrive data
      * @param bool $planning
      * 
-	 * @return JsonResponse
+     * @return JsonResponse
      */
     public function debit(bool $planning): JsonResponse
     {
@@ -164,17 +167,18 @@ class StatsController extends Controller
         $entryOld = new DebitService();
         $entryOld->setDateStart($this->startDatePassed)->setDateEnd($this->endDatePassed);
 
-        if($planning === true) {
+        if ($planning === true) {
             $entry->setPlanning($planning);
             $entryOld->setPlanning($planning);
         } else {
             $entry->setPlanning(false);
         }
 
-        return response()->json(new ResponseService(
-            $this->buildResponse($entry->get(),$entryOld->get()))
+        return response()->json(
+            new ResponseService(
+                $this->buildResponse($entry->get(), $entryOld->get())
+            )
         );
-        
     }
 
     /**
@@ -190,23 +194,24 @@ class StatsController extends Controller
 
         $entry = new EntryService();
         $entry->addConditions('id', '>', $lastRow->lastrow);
+        $entry->addConditions('installment', 0);
 
         $entry->setPlanning(false);
 
         $total = MathHelper::sum($entry->get()) + $lastRow->amount;
 
-        if($planning === true) {
+        if ($planning === true) {
             $plannedEntry = MathHelper::sum($this->getPlannedEntry());
             $total += $plannedEntry;
-        } 
+        }
 
-        return response()->json(new ResponseService(
-            [
-                'total' => $total,
-            ]
-        )
+        return response()->json(
+            new ResponseService(
+                [
+                    'total' => $total,
+                ]
+            )
         );
-
     }
 
     /**
@@ -214,7 +219,8 @@ class StatsController extends Controller
      * 
      * @return \Illuminate\Database\Eloquent\Collection
      */
-    private function getPlannedEntry(): \Illuminate\Database\Eloquent\Collection {
+    private function getPlannedEntry(): \Illuminate\Database\Eloquent\Collection
+    {
         $dateTime = new DateTime('now');
         $dateTime = $dateTime->modify('Last day of this month');
         $dateTime = $dateTime->format('Y-m-d H:i:s');
@@ -222,10 +228,11 @@ class StatsController extends Controller
         $dateTimeFirst = new DateTime('now');
         $dateTimeFirst = $dateTimeFirst->modify('First day of this month');
         $dateTimeFirst = $dateTimeFirst->format('Y-m-d H:i:s');
-        
+
         //TODO: use a service
         $entry = Entry::where('planned', 1)->where('date_time', '<=', $dateTime)
-        ->where('date_time', '>=', $dateTimeFirst)->get();
+            ->where('date_time', '>=', $dateTimeFirst)
+            ->where('installment', 0)->get();
 
         return $entry;
     }
@@ -237,14 +244,14 @@ class StatsController extends Controller
     {
         $accounts = Account::all();
         $response = [];
-        foreach($accounts as $account) {
+        foreach ($accounts as $account) {
             $lastRow = $this->getActionConfigurations($account->id);
 
             $entry = new EntryService();
             $entry->addConditions('account_id', $account->id);
             $entry->addConditions('id', '>', $lastRow->lastrow);
 
-            if($planning === true) {
+            if ($planning === true) {
                 $entry->setPlanning($planning);
             } else {
                 $entry->setPlanning(false);
@@ -257,16 +264,14 @@ class StatsController extends Controller
                 'account_id' => $account->id,
                 'account_label' => $account->name,
                 'color' => $account->color,
-                'total_wallet' => round($mathTotal->sum() + $lastRow->amount,2)
+                'total_wallet' => round($mathTotal->sum() + $lastRow->amount, 2)
             ];
-
         }
 
 
         return response()->json(new ResponseService(
             $response
         ));
-
     }
 
     /**
@@ -288,31 +293,59 @@ class StatsController extends Controller
         $secondValue = $mathTotalPassed->sum();
 
         return [
-            'total' => round($firstValue,2),
+            'total' => round($firstValue, 2),
             'total_passed' => $secondValue,
             'percentage' => MathHelper::percentage($firstValue, $secondValue)
         ];
     }
 
-   /**
-   * get wallet fix 78187.79;
-   * @param int $account_id 
-   * @return \stdClass
-   */
-  private function getActionConfigurations(int $account_id):\stdClass
-  {
-    $fix = ActionJobConfiguration::where("action", Action::Configurations->value)->orderBy("id", "desc")->get('config');
-    $config = json_decode(
-      '{"account_id":' . $account_id . ',"amount":"0","lastrow":1}'
-    );
-    foreach ($fix as $f) {
-      $config = json_decode($f->config);
-      if ($config->account_id == $account_id) {
+    /**
+     * get wallet fix 78187.79;
+     * @param int $account_id 
+     * @return \stdClass
+     */
+    private function getActionConfigurations(int $account_id): \stdClass
+    {
+        $fix = ActionJobConfiguration::where("action", Action::Configurations->value)->orderBy("id", "desc")->get('config');
+        $config = json_decode(
+            '{"account_id":' . $account_id . ',"amount":"0","lastrow":1}'
+        );
+        foreach ($fix as $f) {
+            $config = json_decode($f->config);
+            if ($config->account_id == $account_id) {
+                return $config;
+            }
+        }
+
         return $config;
-      }
     }
 
-    return $config;
-  }
+    /**
+     * get the healt of wallet
+     */
+    public function health(bool $planned): JsonResponse
+    {
+        $lastRow = $this->getActionConfigurations(0);
 
+        $dateTime = new DateTime('now');
+        $dateTime = $dateTime->modify('Last day of this month');
+        $dateTime = $dateTime->format('Y-m-d H:i:s');
+
+        $entry = new EntryService();
+        $entry->addConditions('id', '>', $lastRow->lastrow);
+        $entry->setPlanning(false);
+
+        $total = MathHelper::sum($entry->get()) + $lastRow->amount;
+
+        $plannedEntry = MathHelper::sum($this->getPlannedEntry());
+        $total += $plannedEntry;
+
+        return response()->json(
+            new ResponseService(
+                [
+                    'total' => $total,
+                ]
+            )
+        );
+    }
 }
