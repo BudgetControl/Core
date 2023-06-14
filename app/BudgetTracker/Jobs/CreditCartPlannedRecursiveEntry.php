670d11a66e5537d21573ce955e5429c6af69fe53
Marco De Felice <marco.defelice@consulenti.eolo.it>
2023-06-14 11:08:34 +0200

    added new job for ricursive entry

---
+++ app/BudgetTracker/Jobs/CreditCartPlannedRecursiveEntry.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\BudgetTracker\Jobs;
+
+use App\BudgetTracker\Entity\Entries\Entry as EntriesEntry;
+use App\BudgetTracker\Models\Account;
+use App\BudgetTracker\Models\Currency;
+use App\BudgetTracker\Models\PaymentsTypes;
+use App\BudgetTracker\Services\ExpensesService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\BudgetTracker\Models\Entry;
+use App\BudgetTracker\Models\SubCategory;
+use Illuminate\Support\Facades\Log;
+
+class CreditCartPlannedRecursiveEntry implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        $this->handle();
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try {
+
+            $accounts = $this->findCreditCards();
+            foreach($accounts as $account) {
+                $entry = $this->entry($account->installementValue,$account);
+
+                if($this->exist($entry->getNote()) === false) {
+                    $service = new ExpensesService();
+                    $service->save($entry->toArray());
+                }
+            }
+
+        } catch (\Exception $e) {
+            Log::error('Error while insert new credit card recursive entry '.$e->getMessage());
+        }
+    }
+
+    /**
+     * find planned entries
+     * @return Account
+     */
+    private function findCreditCards() : Account
+    {   
+        return Account::where('installement',1)->get();
+    }
+
+    private function entry(float $amount,Account $account): EntriesEntry
+    {
+        $date = new \DateTime($account->date);
+        $day = $date->format('d');
+        $newDate = date('Y',time()).'-'.date('m',time()).'-'.$day;
+
+        return new EntriesEntry(
+            $amount,
+            Currency::find(11),
+            $account->name . ' '. $newDate,
+            SubCategory::find(55),
+            $account,
+            PaymentsTypes::find(1),
+            new \DateTime($newDate),
+        );
+    }
+
+    private function exist($note): bool
+    {
+        $entry = Entry::where('note', $note)->get();
+        if(empty($entry)) {
+            return false;
+        }
+        return true;
+    }
+}
