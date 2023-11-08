e9a3aa0e5b8cdc566e89c6872d43329c64a5f0f6
Marco De Felice <marco.defelice@qwentes.it>
2023-11-08 17:24:40 +0100

    new configuration

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 composer install --ignore-platform-reqs --optimize-autoloader
 
-php artisan serve --host 0.0.0.0 --port=3000
 php artisan config:cache
 php artisan optimize:clear
