e823febaf6a0a8726433c60fc1c00452eb8d82fb
Marco De Felice <marco.defelice@qwentes.it>
2023-11-10 12:08:49 +0100

    script

--- bin/scripts/InstallApplication.sh
+++ bin/scripts/InstallApplication.sh
@@ -2,7 +2,9 @@
 
 echo "Install application"
 cd /var/www/app && docker-compose up -d
+cp /var/www/.env /var/www/app/.env
 docker exec budgetcontrol-services bash -c "composer install"
+docker exec budgetcontrol-services bash -c "php artisan config:cache"
 docker exec budgetcontrol-services bash -c "php artisan migrate"
 docker exec budgetcontrol-services bash -c "cd /var/www/workdir && chown -R www-data:www-data *" #apache group
 docker exec budgetcontrol-services bash -c "cd /var/www/workdir && chmod 775 -R *" #apache group
