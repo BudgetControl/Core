78f41572ea4a084b875340a4f4098a2197851556
Marco De Felice <marco.defelice@qwentes.it>
2024-01-23 14:32:05 +0100

    fixed all unit tests

--- bin/scripts/phpunitTest.sh
+++ bin/scripts/phpunitTest.sh
@@ -2,6 +2,6 @@ cp ../../.env.testing ../../.env
 docker exec budgetcontrol-core php artisan config:cache
 docker exec budgetcontrol-core php artisan migrate:fresh --path database/migrations --path database/migrations/users
 docker exec budgetcontrol-core php artisan db:seed
-docker exec budgetcontrol-core php artisan test
+docker exec budgetcontrol-core vendor/bin/phpunit
 cp ../../.env.bk ../../.env
 docker exec budgetcontrol-core php artisan config:cache
