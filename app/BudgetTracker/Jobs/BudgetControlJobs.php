6fcad45cf26dbd5dc2effe9ab50724f1ec0cbe21
Marco De Felice <marco.defelice@qwentes.it>
2024-01-24 13:54:56 +0100

    fix JOBS schedule

--- app/BudgetTracker/Jobs/BudgetControlJobs.php
+++ app/BudgetTracker/Jobs/BudgetControlJobs.php
@@ -31,7 +31,7 @@ abstract class BudgetControlJobs
 
     }
 
-    protected function switchDatabase(array $dbName)
+    protected function switchDatabase(string $dbName)
     {
         $database_name = $dbName;
         try {
