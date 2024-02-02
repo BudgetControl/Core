a03c7e91eae76b6f9edc649efac5591075fcf695
Marco De Felice <marco.defelice@qwentes.it>
2024-02-02 23:26:14 +0100

    added sub columnt to table

--- app/Auth/Middleware/AuthCognitoMiddleware.php
+++ app/Auth/Middleware/AuthCognitoMiddleware.php
@@ -14,6 +14,7 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class AuthCognitoMiddleware
 {
@@ -30,69 +31,45 @@ class AuthCognitoMiddleware
     public function handle($request, \Closure $next)
     {
         /** only fot php unit testting */
-        if(@$_ENV['DISABLE_AUTH'] == true) {
+        if (@$_ENV['DISABLE_AUTH'] == true) {
             UserService::setUserCache(User::find(1));
             return $next($request);
         }
 
-        $accessToken = str_replace('Bearer ','',$request->header('authorization'));
-        $accessToken = str_replace('Bearer ','',$request->header('authorization'));
+        $accessToken = str_replace('Bearer ', '', $request->header('authorization'));
+        $accessToken = str_replace('Bearer ', '', $request->header('authorization'));
 
-        $refreshToken = Cache::create($accessToken.'refresh_token')->get();
+        $refreshToken = Cache::create($accessToken . 'refresh_token')->get();
         $accessToken = AccessToken::set($accessToken);
 
         try {
             $jwtToken = new JwtToken();
-            $decodedAccessToken = $jwtToken->decode($accessToken->value());
+            $jwtToken->decode($accessToken->value());
 
-            $expirationTime = $decodedAccessToken['exp'];
-            $currentTimestamp = time();
-
-            if ($expirationTime < $currentTimestamp) {
-                // token expired
-                /** @var \App\Auth\Entity\Cognito\RefreshToken $refreshToken */
-                if ($refreshToken) {
-                    try {
-                        $accessTokenString = $accessToken->value();
-                        // try with refresh token
-                        $result = CognitoClientService::init($decodedAccessToken['sub'])->refresh($refreshToken->value());
-                        $request->headers->set('authorization',$result->getToken(CognitoToken::ACCESS)->value(),true);
-                        $this->authenticate($accessToken);
-
-                        return $next($request);
-
-                      
-                    } catch (\Exception $e) {
-                        // Gestisci le eccezioni durante la decodifica o la verifica del refresh token
-                        Log::error('Error: ' . $e->getMessage());
-                        return response()->json([
-                            "message" => "Not authorized"
-                        ], 401);
-                    }
-                } else {
-                    return response()->json([
-                        "message" => "Refresh token not valid"
-                    ], 401);
-                }
-            } else {
-                $this->authenticate($accessToken);
-                return $next($request);
-            }
+            return $request($next);
         } catch (\Exception $e) {
-            // Gestisci le eccezioni durante la decodifica o la verifica del token di accesso
-            Log::error('Error: ' . $e->getMessage());
-            return response()->json([
-                "message" => "Not authorized"
-            ], 401);
+
+            try {
+                $user = Cache::create($accessToken->value())->get();
+                $sub = $user->sub;
+                // try with refresh token
+                $result = CognitoClientService::init($sub)->refresh($refreshToken->value());
+                $request->headers->set('authorization', $result->getToken(CognitoToken::ACCESS)->value(), true);
+
+                UserService::setUserCache($user);
+                UserService::setTokenCache($accessToken);
+
+                return $next($request);
+
+            } catch (Throwable $e) {
+
+                // Gestisci le eccezioni durante la decodifica o la verifica del token di accesso
+                Log::error('Error: ' . $e->getMessage());
+                return response()->json([
+                    "message" => "Not authorized"
+                ], 401);
+            }
         }
-
     }
 
-    private function authenticate(AccessToken $accessToken)
-    {
-        $user = Cache::create($accessToken->value())->get();
-        UserService::setUserCache($user);
-        UserService::setTokenCache($accessToken);
-
-    }
 }
