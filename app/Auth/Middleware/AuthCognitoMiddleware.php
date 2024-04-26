4bad06303a3cf20e136eed97f67b2d8f489bad06
Marco De Felice <=>
2024-04-26 10:15:36 +0200

    removed cognito check becouse now into Gateway will be che ckeck

--- app/Auth/Middleware/AuthCognitoMiddleware.php
+++ app/Auth/Middleware/AuthCognitoMiddleware.php
@@ -40,49 +40,12 @@ class AuthCognitoMiddleware
         $user = User::where('sub',$sub)->first();
         UserService::setUserCache($user);
 
-        /** only fot php unit testting */
-        $accessToken = str_replace('Bearer ', '', $request->query('auth'));
-        $refreshToken = Cache::create($accessToken . 'refresh_token')->get();
-
-        $accessToken = AccessToken::set($accessToken);
-
-        try {
-            $jwtToken = new JwtToken();
-            $jwtToken->decode($accessToken->value());
-
-            UserService::setUserCache($user);
-            $response = $next($request);
-            $response->headers->set('Authorization', "Bearer ".$accessToken->value(), true);
-            return $response;
-        } catch (\Exception $e) {
-
-            //if token is expired delete it
-            Cache::create($accessToken->value())->delete();
-
-            try {
-                $sub = $user->sub;
-                // try with refresh token
-                $result = CognitoClientService::init($sub)->refresh($refreshToken->value());
-
-                $newAccessToken = $result->getToken(CognitoToken::ACCESS)->value();
-                UserService::setUserCache($user);
-                Cache::create($newAccessToken)->set($user,CACHE::TTL_FOREVER);
-
-                $response = $next($request);
-                $response->headers->set('Authorization', "Bearer ".$newAccessToken, true);
-
-                return $response;
-
-            } catch (Throwable $e) {
-
-                // Gestisci le eccezioni durante la decodifica o la verifica del token di accesso
-                Log::error('Error: ' . $e->getMessage());
-                Log::debug("token ::: ".$accessToken->value());
-                return response()->json([
-                    "message" => "Not authorized"
-                ], 401);
-            }
+        if(empty($user)) {
+            return response('Unauthorized', 401);
         }
+
+        return $next($request);
+
     }
 
 }
