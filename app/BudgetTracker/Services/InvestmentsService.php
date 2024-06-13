7f1e433b03d2cb951d3d5b2d3573d11f7a12e863
Marco De Felice <marco.defelice890@gmail.com>
2024-06-13 18:27:12 +0200

    super hotfix

--- app/BudgetTracker/Services/InvestmentsService.php
+++ app/BudgetTracker/Services/InvestmentsService.php
@@ -69,7 +69,7 @@ class InvestmentsService extends EntryService
             $entryModel->amount = $entry->getAmount();
             $entryModel->category_id = $entry->getCategory()->id;
             $entryModel->currency_id = $entry->getCurrency()->id;
-            $entryModel->date_time = Carbon::parse($entry->getDateFormat())->toAtomString();
+            $entryModel->date_time = $entry->getDateFormat();
             $entryModel->note = $entry->getNote();
             $entryModel->payment_type = $entry->getPaymentType()->id;
             $entryModel->planned = $entry->getPlanned();
